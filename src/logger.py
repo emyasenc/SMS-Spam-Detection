dcd1de18f84a2b4f5aa5e3a4fc0dbbed91822e36
Emma Yasenchak <emyasenc@mtu.edu>
2024-11-12 18:24:05 -0500

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,31 +1,26 @@
 import os
 import logging
 
-def get_logger(log_file="predict_pipeline.log"):
+def get_logger(log_path="logs/predict_pipeline.log"):
     # Ensure the logs directory exists
-    log_dir = os.path.join(os.path.dirname(__file__), 'logs')
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
 
-    log_path = os.path.join(log_dir, log_file)
-    
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    
+
     # Create file handler for logging to a file
     file_handler = logging.FileHandler(log_path)
     file_handler.setLevel(logging.DEBUG)
-    
+
     # Create console handler for logging to the console
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
-    
+
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-    
-    return logger
 
+    return logger
